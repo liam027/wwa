c3a874038a4889fadd5f169a4d19dc1efc910b46
liam027 <liam027@gmail.com>
2019-09-18 07:21:06 -0400

    remove test comment from Main

--- src/main/java/com/worldwarofants/game/Main.java
+++ src/main/java/com/worldwarofants/game/Main.java
@@ -2,8 +2,6 @@ package com.worldwarofants.game;
 
 public class Main {
 
-
-    //test comment 9/14/2019
     public static void main(String[] args) {
         ConsoleGame game = new ConsoleGame();
         game.run();

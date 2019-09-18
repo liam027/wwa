482eb57df5819ff2cb143f70f6d870a1a9bc4064
liam027 <liam027@gmail.com>
2019-09-18 09:59:27 -0400

    add PlayerService

---
+++ src/main/java/com/worldwarofants/game/service/PlayerService.java
@@ -0,0 +1,16 @@
+package com.worldwarofants.game.service;
+
+import com.worldwarofants.game.model.World;
+import com.worldwarofants.game.model.Player;
+
+/**
+* Provides service for accessing player data
+ */
+public class PlayerService {
+
+    private Player player;
+
+    public PlayerService(World world) {
+        player = world.player;
+    }
+}

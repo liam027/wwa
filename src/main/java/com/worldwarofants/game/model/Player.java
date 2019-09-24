e98d4b87dc44fff0edc41dc674796a20ff9dc842
liam027 <liam027@gmail.com>
2019-09-24 07:14:24 -0400

    Player: make properties private and add getters

--- src/main/java/com/worldwarofants/game/model/Player.java
+++ src/main/java/com/worldwarofants/game/model/Player.java
@@ -1,33 +1,36 @@
 package com.worldwarofants.game.model;
-
-import java.util.HashMap;
 /**
  * Player's class contains all information regarding player's resources and status
  */
 public class Player {
-
-    protected int population;
-    protected int hitPoints;
-    protected int food;
-    protected int resources;
+    //TODO this player class represents the user, but we will
+    //need other army teams as enemies. Potentially make a super
+    //class "AntTeam" or something that player and opponents can extend
+    //from?
+    private int population;
+    private int hitPoints;
+    private int food;
+    private int resources;
 
     public Player() {
+        //TODO what these properties are and represent TBD by game design. Placeholder
+        //properties from Issue #115 used currently. Default values are arbitrary at this time.
         this.population = 350;
         this.hitPoints = 10;
         this.food = 18;
         this.resources = 0;
     }
 
-    /**
-     * return the four core player attributes as a hashmap of <"attribute name", value>
-     */
-    public HashMap<String, Integer> getAttributes() {
-        HashMap<String, Integer> attributeHashMap = new HashMap<String, Integer>();
-        attributeHashMap.put("population", this.population);
-        attributeHashMap.put("hitPoints", this.hitPoints);
-        attributeHashMap.put("food", this.food);
-        attributeHashMap.put("resources", this.resources);
-        return attributeHashMap;
+    public int getPopulation(){
+        return this.population;
     }
-
+    public int getHitPoints(){
+        return this.hitPoints;
+    }
+    public int getFood(){
+        return this.food;
+    }
+    public int getResources(){
+        return this.resources;
+    }
 }

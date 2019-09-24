0bc98e0be81aede68f646453d1b813d44665a10c
liam027 <liam027@gmail.com>
2019-09-24 08:00:29 -0400

    remove world reference from NewGame and StartMenu view models

--- src/main/java/com/worldwarofants/game/module/newgame/NewGameViewModel.java
+++ src/main/java/com/worldwarofants/game/module/newgame/NewGameViewModel.java
@@ -1,7 +1,6 @@
 package com.worldwarofants.game.module.newgame;
 
 import com.worldwarofants.game.arch.IViewModel;
-import com.worldwarofants.game.model.World;
 
 public class NewGameViewModel implements IViewModel {
 
--- src/main/java/com/worldwarofants/game/module/startmenu/StartMenuViewModel.java
+++ src/main/java/com/worldwarofants/game/module/startmenu/StartMenuViewModel.java
@@ -1,7 +1,7 @@
 package com.worldwarofants.game.module.startmenu;
 
 import com.worldwarofants.game.arch.IViewModel;
-import com.worldwarofants.game.model.World;
+
 public class StartMenuViewModel implements IViewModel {
 
 }

e209cece68fe06e614f5b1a75d66b2ba24c405b0
liam027 <liam027@gmail.com>
2019-09-24 12:42:41 -0400

    tidied Player comment

--- src/main/java/com/worldwarofants/game/model/Player.java
+++ src/main/java/com/worldwarofants/game/model/Player.java
@@ -7,19 +7,21 @@ public class Player {
     //need other army teams as enemies. Potentially make a super
     //class "AntTeam" or something that player and opponents can extend
     //from?
+   
     private final static int STARTING_POPULATION = 350;
     private final static int STARTING_HIT_POINTS = 10;
     private final static int STARTING_FOOD = 18;
     private final static int STARTING_RESOURCES = 0;
 
+    //TODO what these properties are and represent TBD by game design. Placeholder
+    //properties from Issue #115 used currently. Default values are arbitrary at this time.
     private int population;
     private int hitPoints;
     private int food;
     private int resources;
 
     public Player() {
-        //TODO what these properties are and represent TBD by game design. Placeholder
-        //properties from Issue #115 used currently. Default values are arbitrary at this time.
+        
         this.population = STARTING_POPULATION;
         this.hitPoints = STARTING_HIT_POINTS;
         this.food = STARTING_FOOD;
